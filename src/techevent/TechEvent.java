3dd82b855b5cb266cdea16317b987dc0e645052e
YoussefJaballi <youssef.jaballi@esprit.tn>
2019-05-30 14:30:12 +0200

    test

--- src/techevent/TechEvent.java
+++ src/techevent/TechEvent.java
@@ -5,6 +5,8 @@
  */
 package techevent;
 
+import edu.esprit.models.Location;
+import edu.esprit.services.implementation.LocationService;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,15 +29,18 @@ public class TechEvent  {
     public static void main(String[] args) {
          try (InputStream input = new FileInputStream("Database.properties")) {
 
-            Properties prop = new Properties();
+           // Properties prop = new Properties();
 
             // load a properties file
-            prop.load(input);
+            //prop.load(input);
 
             // get the property value and print it out
-            System.out.println(prop.getProperty("url"));
+            /*System.out.println(prop.getProperty("url"));
             System.out.println(prop.getProperty("user"));
-            System.out.println(prop.getProperty("password"));
+            System.out.println(prop.getProperty("password"));*/
+            
+            LocationService l = new LocationService();
+            l.create(new Location(1,"test",36.10,10.10,2087));
 
         } catch (IOException ex) {
             
